9c49d9cdcc938cc09761be8a3ff2a7e66421ee2c
BernardoVxexra <bernardovieira200308@gmail.com>
2025-09-01 22:47:13 -0300

    progesso

--- Mivick/app/iindex.tsx
+++ Mivick/app/iindex.tsx
@@ -2,15 +2,15 @@ import { View, Button } from 'react-native';
 import { useRouter } from 'expo-router';
 import "./global.css";
 
-export default function Index() {
-  const router = useRouter();
+ // export default function Index() {
+//  const router = useRouter();
 
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Button title="Conectar dispositivo" onPress={() => router.push('/ble-screen')} />
-      <Button title="Hístorico " onPress={() => router.push('/historico')} />
-    </View>
-  );
-}
+  //return (
+   // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    //  <Button title="Conectar dispositivo" onPress={() => router.push('/ble-screen')} />
+    //  <Button title="Hístorico " onPress={() => router.push('/historico')} />
+   // </View>
+//  );
+// }
 
 // eas build -p android --profile production  (gera o app)
--- Mivick/app/index.tsx
+++ Mivick/app/index.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { useRouter } from 'expo-router';
 import { FirstButton } from '@/components/FirstButton/FirstButton';
+import "./global.css";
+
 
 const index = () => {
   const router = useRouter();
@@ -10,7 +12,7 @@ const index = () => {
   return (
     <View className="flex-1 bg-background">
       <Image
-        source={require('../assets/carousel-1.jpg')}
+        source={require('../assets/Icons/icon.png')}
         className="absolute w-full h-full"
         resizeMode="cover"
       />
@@ -18,8 +20,8 @@ const index = () => {
         <Text className="text-white text-center text-4xl font-sans-bold-pro mb-8">
           Mivick
         </Text>
-        <FirstButton text="Entrar" onPress={() => router.push('/(screens)/home')} />
-        <TouchableOpacity className="mt-4 items-center" onPress={() => router.push('/(screens)/home')}>
+        <FirstButton text="Entrar" onPress={() => router.push('/index')} />
+        <TouchableOpacity className="mt-4 items-center" onPress={() => router.push('register')}>
           <Text className="text-primary font-sans-bold-pro text-lg">
             Cadastre-se
           </Text>
@@ -30,3 +32,5 @@ const index = () => {
 };
 
 // eas build -p android --profile production  (gera o app)
+
+export default index; 
